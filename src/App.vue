f2c70acff5529459ed320d3f699c84682e054fac
oneselfworld <940451598@qq.com>
2021-04-19 01:01:39 +0800

    A

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <keep-alive exclude="Detail">
+      <router-view></router-view>
+    </keep-alive>
+    <main-tab-bar></main-tab-bar>
+    <icon></icon>
+    <svg-icon></svg-icon>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import MainTabBar from 'content/mainTabbar/MainTabBar'
+  import Icon from 'content/Icon/Icon.vue'
+  import SvgIcon from 'content/Icon/svg.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar,
+      Icon,
+      SvgIcon
+    }
   }
-}
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  @import "assets/css/base.css";
+
+  #app {
+    position: relative;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueLazyLoad from 'vue-lazyload'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  preLoad: 1,
+  loading: require('assets/img/common/placeholder.png')
+})
+
+new Vue({
+  render: h => h(App),
+  store,
+  router
+}).$mount('#app')
